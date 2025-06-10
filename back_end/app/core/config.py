fa96b7742e382cee8be7d3157dc86fd93e621ecb
tuananhudu <buiduytuananhudu@gmail.com>
2025-06-10 16:05:12 +0700

    Cấu hình môi trường database

--- back_end/app/core/config.py
+++ back_end/app/core/config.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+## Set up từ file môi trường đây là những thữ nhạy cảm ở file .env mà sẽ kh up lên git 
 MONGO_URI = os.getenv("MONGO_URI")
 JWT_SECRET = os.getenv("JWT_SECRET")
